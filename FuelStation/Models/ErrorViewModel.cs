bd70d7f91215b73fc70224114977b234c60e0561
Oleg <olgasn@inbox.ru>
2019-03-18 10:45:49 +0300

    .NET Core 2.1

---
+++ FuelStation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FuelStation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
