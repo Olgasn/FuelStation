50a77ff4a213520f3b0e4bda5d7a5a489839c0b2
Asenchik Oleg <olgasn@inbox.ru>
2019-10-13 22:55:52 +0300

    eeee

--- FuelStation/Startup.cs
+++ FuelStation/Startup.cs
@@ -7,6 +7,7 @@ using FuelStation.Middleware;
 using FuelStation.Data;
 using FuelStation.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 namespace FuelStation
 {
@@ -39,39 +40,24 @@ namespace FuelStation
             // добавление поддержки сессии
             services.AddDistributedMemoryCache();
             services.AddSession();
+            services.AddControllersWithViews();
 
-            services.AddMvc(options =>
-            {
-                // определение профилей кэширования
-                options.CacheProfiles.Add("Caching",
-                    new CacheProfile()
-                    {
-                        Duration = 30
-                    });
-                options.CacheProfiles.Add("NoCaching",
-                    new CacheProfile()
-                    {
-                        Location = ResponseCacheLocation.None,
-                        NoStore = true
-                    });
-            });
         }
 
         // Этот метод вызывается во время выполнения. Используйте этот метод для настройки конвейера HTTP-запросов.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            // добавляем поддержку статических файлов
-            app.UseStaticFiles();
+                // добавляем поддержку статических файлов
+                app.UseStaticFiles();
 
             // добавляем поддержку сессий
             app.UseSession();
@@ -82,13 +68,16 @@ namespace FuelStation
             // добавляем компонент middleware для реализации кэширования и записывем данные в кэш
             app.UseOperatinCache("Operations 10");
 
-            app.UseMvc((routes) =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            }
-            );
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
 
         }
     }
