c6e60b3aee8f4eb676c87431c69fcfd8c5ff50d5
olgasn@inbox.ru <olgasn@inbox.ru>
2019-11-19 11:25:16 +0300

    User Views

--- FuelStation/Startup.cs
+++ FuelStation/Startup.cs
@@ -37,7 +37,7 @@ namespace FuelStation
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            string connectionDB = Configuration.GetConnectionString("DefaultConnection");
+            string connectionDB = Configuration.GetConnectionString("SqlServerConnection");
             services.AddDbContext<FuelsContext>(options => options.UseSqlServer(connectionDB));
             string connectionUsers = Configuration.GetConnectionString("IdentityConnection");
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionUsers));
