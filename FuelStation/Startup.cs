94dc02c5125d493b3215dad569561f6049eae0d9
Asenchik Oleg <olgasn@inbox.ru>
2022-10-16 20:52:20 +0300

    Update to .NET 6.0

--- FuelStation/Startup.cs
+++ FuelStation/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Collections.Generic;
-using System.Globalization;
 
 namespace FuelStation
 {
