098ecd5036c915d40b75c695c2f265b5b08413b4
olgasn@inbox.ru <olgasn@inbox.ru>
2023-10-11 12:50:01 +0300

    Remove Startup

--- FuelStation/Program.cs
+++ FuelStation/Program.cs
@@ -1,21 +1,177 @@
-п»їusing Microsoft.AspNetCore.Hosting;
+using FuelStation.Data;
+using FuelStation.Infrastructure;
+using FuelStation.Middleware;
+using FuelStation.Models;
+using FuelStation.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
 
-namespace FuelStation
+namespace FuelStationT
 {
     public class Program
     {
-        //РўРѕС‡РєР° РІС…РѕРґР°
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            var services = builder.Services;
+            // внедрение зависимости для доступа к БД с использованием EF
+            string connection = builder.Configuration.GetConnectionString("SqlServerConnection");
+            services.AddDbContext<FuelsContext>(options => options.UseSqlServer(connection));
+
+            // добавление кэширования
+            services.AddMemoryCache();
+
+            // добавление поддержки сессии
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+
+            // внедрение зависимости CachedTanksService
+            services.AddScoped<ICachedTanksService, CachedTanksService>();
+
+            //Использование MVC - отключено
+            //services.AddControllersWithViews();
+            var app = builder.Build();
+
+
+            // добавляем поддержку статических файлов
+            app.UseStaticFiles();
+
+            // добавляем поддержку сессий
+            app.UseSession();
+
+            // добавляем собственный компонент middleware по инициализации базы данных и производим ее инициализацию
+            app.UseDbInitializer();
+
+
+            //Запоминание в Session значений, введенных в форме
+            app.Map("/form", (appBuilder) =>
+            {
+                appBuilder.Run(async (context) =>
                 {
-                    webBuilder.UseStartup<Startup>();
+
+                    // Считывание из Session объекта User
+                    User user = context.Session.Get<User>("user") ?? new User();
+
+                    // Формирование строки для вывода динамической HTML формы
+                    string strResponse = "<HTML><HEAD><TITLE>Пользователь</TITLE></HEAD>" +
+                    "<META http-equiv='Content-Type' content='text/html; charset=utf-8'/>" +
+                    "<BODY><FORM action ='/form' / >" +
+                    "Имя:<BR><INPUT type = 'text' name = 'FirstName' value = " + user.FirstName + ">" +
+                    "<BR>Фамилия:<BR><INPUT type = 'text' name = 'LastName' value = " + user.LastName + " >" +
+                    "<BR><BR><INPUT type ='submit' value='Сохранить в Session'><INPUT type ='submit' value='Показать'></FORM>";
+                    strResponse += "<BR><A href='/'>Главная</A></BODY></HTML>";
+
+                    // Запись в Session данных объекта User
+                    user.FirstName = context.Request.Query["FirstName"];
+                    user.LastName = context.Request.Query["LastName"];
+                    context.Session.Set<User>("user", user);
+
+                    // Асинхронный вывод динамической HTML формы
+                    await context.Response.WriteAsync(strResponse);
                 });
+            });
+
+
+
+            //Запоминание в Сookies значений, введенных в форме
+            //...
+
+
+            // Вывод информации о клиенте
+            app.Map("/info", (appBuilder) =>
+            {
+                appBuilder.Run(async (context) =>
+                {
+                    // Формирование строки для вывода 
+                    string strResponse = "<HTML><HEAD><TITLE>Информация</TITLE></HEAD>" +
+                    "<META http-equiv='Content-Type' content='text/html; charset=utf-8'/>" +
+                    "<BODY><H1>Информация:</H1>";
+                    strResponse += "<BR> Сервер: " + context.Request.Host;
+                    strResponse += "<BR> Путь: " + context.Request.PathBase;
+                    strResponse += "<BR> Протокол: " + context.Request.Protocol;
+                    strResponse += "<BR><A href='/'>Главная</A></BODY></HTML>";
+                    // Вывод данных
+                    await context.Response.WriteAsync(strResponse);
+                });
+            });
+
+            // Вывод кэшированной информации из таблицы базы данных
+            app.Map("/tanks", (appBuilder) =>
+            {
+                appBuilder.Run(async (context) =>
+                {
+                    //обращение к сервису
+                    ICachedTanksService cachedTanksService = context.RequestServices.GetService<ICachedTanksService>();
+                    IEnumerable<Tank> tanks = cachedTanksService.GetTanks("Tanks20");
+                    string HtmlString = "<HTML><HEAD><TITLE>Емкости</TITLE></HEAD>" +
+                    "<META http-equiv='Content-Type' content='text/html; charset=utf-8'/>" +
+                    "<BODY><H1>Список емкостей</H1>" +
+                    "<TABLE BORDER=1>";
+                    HtmlString += "<TR>";
+                    HtmlString += "<TH>Код</TH>";
+                    HtmlString += "<TH>Материал</TH>";
+                    HtmlString += "<TH>Тип</TH>";
+                    HtmlString += "<TH>Объем</TH>";
+                    HtmlString += "</TR>";
+                    foreach (var tank in tanks)
+                    {
+                        HtmlString += "<TR>";
+                        HtmlString += "<TD>" + tank.TankId + "</TD>";
+                        HtmlString += "<TD>" + tank.TankMaterial + "</TD>";
+                        HtmlString += "<TD>" + tank.TankType + "</TD>";
+                        HtmlString += "<TD>" + tank.TankVolume + "</TD>";
+                        HtmlString += "</TR>";
+                    }
+                    HtmlString += "</TABLE>";
+                    HtmlString += "<BR><A href='/'>Главная</A></BR>";
+                    HtmlString += "<BR><A href='/tanks'>Емкости</A></BR>";
+                    HtmlString += "<BR><A href='/form'>Данные пользователя</A></BR>";
+                    HtmlString += "</BODY></HTML>";
+
+                    // Вывод данных
+                    await context.Response.WriteAsync(HtmlString);
+                });
+            });
+
+
+
+            // Стартовая страница и кэширование данных таблицы на web-сервере
+            app.Run((context) =>
+            {
+                //обращение к сервису
+                ICachedTanksService cachedTanksService = context.RequestServices.GetService<ICachedTanksService>();
+                cachedTanksService.AddTanks("Tanks20");
+                string HtmlString = "<HTML><HEAD><TITLE>Емкости</TITLE></HEAD>" +
+                "<META http-equiv='Content-Type' content='text/html; charset=utf-8'/>" +
+                "<BODY><H1>Главная</H1>";
+                HtmlString += "<H2>Данные записаны в кэш сервера</H2>";
+                HtmlString += "<BR><A href='/'>Главная</A></BR>";
+                HtmlString += "<BR><A href='/tanks'>Емкости</A></BR>";
+                HtmlString += "<BR><A href='/form'>Данные пользователя</A></BR>";
+                HtmlString += "</BODY></HTML>";
+
+                return context.Response.WriteAsync(HtmlString);
+
+            });
+
+            //Использование MVC - отключено
+            //app.UseRouting();
+            //app.UseAuthorization();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
+
+            app.Run();
+        }
     }
 }
