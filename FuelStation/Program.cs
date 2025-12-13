d1fda1ffaa9973831ea229d6895fe68b6c5e1d4e
Asenchik Oleg <olgasn@inbox.ru>
2025-12-13 12:38:54 +0300

    .NEN 10.0

--- FuelStation/Program.cs
+++ FuelStation/Program.cs
@@ -6,7 +6,6 @@ using FuelStation.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
